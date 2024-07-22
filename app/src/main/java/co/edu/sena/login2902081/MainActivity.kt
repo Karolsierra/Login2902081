3aaa6820483b37b12b2f74f1202dd59e19704e40
Karolsierra <karitomfvj12@gmail.com>
2024-07-21 22:21:25 -0500

    Agregada MainActivity que configura el contenido usando postOfficeApp y habilita el modo edge-to-edge

--- app/src/main/java/co/edu/sena/login2902081/MainActivity.kt
+++ app/src/main/java/co/edu/sena/login2902081/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import co.edu.sena.login2902081.app.postOfficeApp
 import co.edu.sena.login2902081.ui.theme.Login2902081Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Login2902081Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            postOfficeApp()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Login2902081Theme {
-        Greeting("Android")
-    }
-}
